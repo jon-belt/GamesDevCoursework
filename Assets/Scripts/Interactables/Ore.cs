084fcca1486316ddf94d2cc7e327353f18b3a8a6
jon-belt <48921235+jon-belt@users.noreply.github.com>
2024-03-31 02:41:38 +0100

    fixed last commit

--- Assets/Scripts/Interactables/Ore.cs
+++ Assets/Scripts/Interactables/Ore.cs
@@ -29,7 +29,7 @@ public class Ore : MonoBehaviour, IInteractable
         //trigger to destroy ore after durability has been depleted
         if (durability <= 0)
         {
-            DestroyAfterDelay();
+            StartCoroutine(DestroyAfterDelay());
         }
         return actualDurabilityReduced;
     }
